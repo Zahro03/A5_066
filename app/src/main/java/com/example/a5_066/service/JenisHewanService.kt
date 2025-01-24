201da9393bc8b28d67c0e1e89e95659ff7e6ee23
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-24 16:03:18 +0700

    Menampilkan detail jenisHewan berdasarkan id

--- app/src/main/java/com/example/a5_066/service/JenisHewanService.kt
+++ app/src/main/java/com/example/a5_066/service/JenisHewanService.kt
@@ -3,6 +3,7 @@ package com.example.a5_066.service
 import com.example.a5_066.model.JenisHewan
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Query
 
 interface JenisHewanService {
     @Headers(
@@ -11,5 +12,9 @@ interface JenisHewanService {
     )
     @GET("bacajenisHewan.php")
     suspend fun getJenisHewan(): List<JenisHewan>
+
+    @GET("bacajenisHewan1.php")
+    suspend fun getJenisHewanById(
+        @Query("id_jenis_hewan") id: String): JenisHewan
 
 }
