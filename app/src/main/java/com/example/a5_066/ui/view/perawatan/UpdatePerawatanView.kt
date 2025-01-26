a119de64bca96e21559a409d0d9cc83b74e7e1f0
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-26 09:47:16 +0700

    Menambahkan layout untuk tampilan update perawatan

--- app/src/main/java/com/example/a5_066/ui/view/perawatan/UpdatePerawatanView.kt
+++ app/src/main/java/com/example/a5_066/ui/view/perawatan/UpdatePerawatanView.kt
@@ -1,10 +1,16 @@
 package com.example.a5_066.ui.view.perawatan
 
 import android.widget.Toast
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun UpdatePerawatanView(
@@ -43,4 +49,12 @@ fun UpdatePerawatanView(
             ).show()
         }
     }
+
+    // Layout untuk tampilan Update Perawatan
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 }
