0105326a49bb0e41a401f1e9cd5d9f527cfb8fb1
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-26 08:39:34 +0700

    Membuat get untuk mendapatkan riwayat berdasarkan id_hewan

---
+++ app/src/main/java/com/example/a5_066/service/PerawatanService.kt
@@ -0,0 +1,17 @@
+package com.example.a5_066.service
+
+import com.example.a5_066.model.Perawatan
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface PerawatanService {
+
+    // Mendapatkan riwayat perawatan berdasarkan id_hewan
+    @GET("perawatan/bacaperawatan/{id_hewan}")
+    suspend fun getRiwayatPerawatan(@Path("id_hewan") id_hewan: String): List<Perawatan>
+}
