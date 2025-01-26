60e97ebc2420d78cabf8ec5b946af21d725c36bf
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-26 09:50:03 +0700

    Menambahkan Text untuk input id hewan

--- app/src/main/java/com/example/a5_066/ui/view/perawatan/UpdatePerawatanView.kt
+++ app/src/main/java/com/example/a5_066/ui/view/perawatan/UpdatePerawatanView.kt
@@ -4,8 +4,10 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,7 +15,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -67,4 +71,15 @@ fun UpdatePerawatanView(
         }
         Spacer(modifier = Modifier.height(16.dp))
     }
+
+    // Input untuk ID Hewan
+    BasicTextField(
+        value = id_hewan.value,
+        onValueChange = { id_hewan.value = it },
+        modifier = Modifier.fillMaxWidth().padding(8.dp),
+        textStyle = TextStyle(color = Color.Black)
+    )
+    Text("ID Hewan", color = Color.Black)
+
+    Spacer(modifier = Modifier.height(8.dp))
 }
