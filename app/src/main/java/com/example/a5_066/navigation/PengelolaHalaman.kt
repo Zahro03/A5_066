6848dc46a4d93246a4447d3710afcc5ab8f70dea
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-26 09:10:50 +0700

    Memanggil perawatan di homePage

--- app/src/main/java/com/example/a5_066/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/a5_066/navigation/PengelolaHalaman.kt
@@ -1,3 +1,5 @@
+@file:Suppress("NAME_SHADOWING")
+
 import android.annotation.SuppressLint
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -7,13 +9,21 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
 import com.example.a5_066.ui.view.Homepage
-import com.example.a5_066.ui.view.dokter.EntryDokterScreen
+import com.example.a5_066.ui.view.dokter.DetailDokterScreen
+import com.example.a5_066.ui.view.dokter.HomeDokterView
+import com.example.a5_066.ui.view.dokter.InsertDokterView
+import com.example.a5_066.ui.view.dokter.UpdateDokterScreen
+import com.example.a5_066.ui.view.jenisHewan.HomejenisHewanView
+import com.example.a5_066.ui.view.jenisHewan.InsertjenisHewanView
 import com.example.a5_066.ui.view.pasien.DestinasiEntry
-import com.example.a5_066.ui.view.pasien.DetailView
-import com.example.a5_066.ui.view.pasien.EntryPasienScreen
-import com.example.a5_066.ui.view.pasien.HomeScreen
+import com.example.a5_066.ui.view.pasien.DestinasiUpdate
+import com.example.a5_066.ui.view.pasien.DetailPasienScreen
+import com.example.a5_066.ui.view.pasien.HomePasienView
+import com.example.a5_066.ui.view.pasien.InsertPasienView
 import com.example.a5_066.ui.view.pasien.UpdatePasienScreen
+import com.example.a5_066.ui.view.perawatan.DestinasiHomePerawatan
 
 @SuppressLint("ComposableDestinationInComposeScope")
 @Composable
@@ -33,6 +43,8 @@ fun PengelolaHalaman(
                     when (item) {
                         "Pasien" -> navController.navigate(DestinasiHomePasien.route)
                         "Dokter" -> navController.navigate(DestinasiHomeDokter.route)
+                        "Jenis Hewan" ->navController.navigate(DestinasiHomejh.route)
+                        "Perawatan" -> navController.navigate(DestinasiHomePerawatan.route)
                         else -> {}
                     }
                 }
@@ -41,39 +53,33 @@ fun PengelolaHalaman(
 
         // Pasien Routes
         composable(DestinasiHomePasien.route) {
-            HomeScreen(
-                navigateToItemEntry = {
-                    navController.navigate(DestinasiInsertPasien.route) // Menavigasi ke halaman tambah pasien
-                },
-                onDetailClick = { id_hewan ->
-                    navController.navigate("${DestinasiDetailPasien.routesWithArg}/$id_hewan")
-                }
+            HomePasienView(
+                onAddPsnClick = { navController.navigate(DestinasiInsertPasien.route) },
+                onBackArrow = { navController.popBackStack() }
             )
         }
 
         composable(DestinasiInsertPasien.route) {
-            EntryPasienScreen(
-                navigateBack = {
-                    navController.popBackStack(DestinasiHome.route, inclusive = true)
-                }
+            InsertPasienView(
+                onBackArrow = { navController.popBackStack() },
+                onNavigate = { navController.popBackStack() }
             )
         }
 
         composable(
             DestinasiDetailPasien.routesWithArg,
-            arguments = listOf(navArgument(DestinasiDetailPasien.id_hewan) { type = NavType.StringType })
-        ) { backStackEntry ->
-            val id_hewan = backStackEntry.arguments?.getString(DestinasiDetailPasien.id_hewan)
-            id_hewan?.let {
-                DetailView(
-                    NavigateBack = {
-                        navController.popBackStack(DestinasiHome.route, inclusive = true)
-                    },
-                    onEditClick = {
-                        navController.navigate("${DestinasiUpdatePasien.routesWithArg}/$it")
-                    },
-                    onDeleteClick = {
-                        navController.popBackStack()
+            arguments = listOf(
+                navArgument(DestinasiDetailPasien.id_hewan) {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            val id_hewan = it.arguments?.getString(DestinasiDetailPasien.id_hewan)
+            id_hewan?.let { id_hewan ->
+                DetailPasienScreen(
+                    navigateBack = { navController.navigateUp() },
+                    onEditClick = { id_hewan ->
+                        navController.navigate("${DestinasiUpdatePasien.route}/$id_hewan")
                     }
                 )
             }
@@ -81,38 +87,96 @@ fun PengelolaHalaman(
 
         composable(
             DestinasiUpdatePasien.routesWithArg,
-            arguments = listOf(navArgument(DestinasiDetailPasien.id_hewan) { type = NavType.StringType })
-        ) { backStackEntry ->
-            val id_hewan = backStackEntry.arguments?.getString(DestinasiDetailPasien.id_hewan)
-            id_hewan?.let {
-                UpdatePasienScreen(
-                    navigateBack = { navController.navigateUp() },
-                    onNavigateUp = {
-                        navController.popBackStack(DestinasiHome.route, inclusive = true)
-                    }
-                )
-            }
+            arguments = listOf(
+                navArgument(DestinasiDetailPasien.id_hewan) {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            UpdatePasienScreen(
+                onBackArrow = { navController.popBackStack() },
+                onNavigate = { navController.popBackStack() }
+            )
         }
 
         // Dokter Routes
         composable(DestinasiHomeDokter.route) {
-            HomeScreen(
-                navigateToItemEntry = {
-                    navController.navigate(DestinasiInsertDokter.route) // Menavigasi ke halaman tambah dokter
-                },
+            HomeDokterView(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
                 onDetailClick = { id_Dokter ->
-                    navController.navigate("${DestinasiDetailDokter.routesWithArg}/$id_Dokter")
+                    navController.navigate("${DestinasiDetailDokter.route}/$id_Dokter")
                 }
             )
         }
 
         composable(DestinasiInsertDokter.route) {
-            EntryDokterScreen(
-                navigateBack = {
-                    navController.popBackStack(DestinasiHome.route, inclusive = true)
+            InsertDokterView(
+                onBackArrow = { navController.popBackStack() },
+                onNavigate = { navController.popBackStack() }
+            )
+        }
+
+        composable(
+            DestinasiDetailDokter.routesWithArg,
+            arguments = listOf(
+                navArgument(DestinasiDetailDokter.id_Dokter) {
+                    type = NavType.StringType
                 }
+            )
+        ) {
+            val id_Dokter = it.arguments?.getString(DestinasiDetailDokter.id_Dokter)
+            id_Dokter?.let { id_Dokter ->
+                DetailDokterScreen(
+                    navigateBack = { navController.navigateUp() },
+                    onEditClick = { id_Dokter ->
+                        navController.navigate("${DestinasiUpdateDokter.route}/$id_Dokter")
+                    }
+                )
+            }
+        }
+
+        composable(
+            DestinasiUpdateDokter.routesWithArg,
+            arguments = listOf(
+                navArgument(DestinasiDetailDokter.id_Dokter) {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            UpdateDokterScreen(
+                onBackArrow = { navController.popBackStack() },
+                onNavigate = { navController.popBackStack() }
+            )
+        }
+
+        // Jenis Hewan Routes
+        composable(DestinasiHomejh.route) {
+            HomejenisHewanView(
+                onAddjenisHewanClick = { navController.navigate(DestinasiInsertjh.route) },
+                onDetailjenisHewanClick = { id_jenis_hewan ->
+                    navController.navigate("${DestinasiDetailjh.route}/$id_jenis_hewan")
+                },
+                onBackArrow = { navController.popBackStack() }
+            )
+        }
+
+        composable(DestinasiInsertjh.route) {
+            InsertjenisHewanView(
+                onBackArrow = { navController.popBackStack() },
+                onNavigate = { navController.popBackStack() }
+            )
+        }
+
+        // Tambah Pasien Deep Link
+        composable(
+            "tambah_pasien",
+            deepLinks = listOf(navDeepLink { uriPattern = "android-app://com.example.a5_066/tambah_pasien" })
+        ) {
+            // Tampilan untuk tambah pasien
+            InsertPasienView(
+                onBackArrow = { navController.popBackStack() },
+                onNavigate = { navController.popBackStack() }
             )
         }
     }
 }
-
