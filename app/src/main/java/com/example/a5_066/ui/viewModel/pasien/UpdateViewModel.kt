2b6c53dc84e5a1831fce08be8e2a0326af77da6d
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-27 21:28:44 +0700

    Menambahkan fungsi pada UpdateViewModel Pasien

--- app/src/main/java/com/example/a5_066/ui/viewModel/pasien/UpdateViewModel.kt
+++ app/src/main/java/com/example/a5_066/ui/viewModel/pasien/UpdateViewModel.kt
@@ -1,43 +1,41 @@
-package com.example.a5_066.ui.viewModel.pasien
-
-import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.a5_066.repository.PasienRepository
+import com.example.a5_066.model.JenisHewan
 import kotlinx.coroutines.launch
 
-class UpdateViewModel(
-  savedStateHandle: SavedStateHandle,
+class UpdatePasienViewModel(
+    savedStateHandle: SavedStateHandle,
     private val pasienRepository: PasienRepository
-): ViewModel() {
-    var updatePasienUiState by mutableStateOf(InsertUiState())
+): ViewModel(){
+    var updateUiState by mutableStateOf(InsertPasienUiState())
         private set
-    val id_hewan: String = checkNotNull(savedStateHandle["id_hewan"])
+
+    var jenisList by mutableStateOf<List<JenisHewan>>(emptyList())
+
+    private val _id_hewan: String = checkNotNull(savedStateHandle[DestinasiUpdatePasien.id_hewan])
 
     init {
         viewModelScope.launch {
-            try {
-                val pasien = pasienRepository.getPasienById(id_hewan)
-                updatePasienUiState = pasien.toUiStatePasien()
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
+            updateUiState = pasienRepository.getPasienById(_id_hewan).data
+                .toUiStatePsn()
         }
     }
 
-    fun updateInsertPasienState(insertUiEvent: InsertUiEvent) {
-        updatePasienUiState = updatePasienUiState.copy(insertUiEvent = insertUiEvent)
+    fun updateState(insertUiEvent: InsertPasienUiEvent){
+        updateUiState = InsertPasienUiState(insertPasienUiEvent = insertUiEvent)
     }
 
-    suspend fun updatePasien() {
-        try {
-            pasienRepository.updatePasien(id_hewan, updatePasienUiState.insertUiEvent.toPasien())
-        } catch (e: Exception) {
-            e.printStackTrace()
+    suspend fun updatePasien(){
+        viewModelScope.launch {
+            try {
+                pasienRepository.updatePasien(_id_hewan, updateUiState.insertPasienUiEvent.toPasien())
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
         }
     }
 }
