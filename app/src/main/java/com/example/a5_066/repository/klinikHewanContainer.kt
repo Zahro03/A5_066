ed461c130edb462d365b6de2106a5350bf24635b
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-27 21:30:30 +0700

    Menambahkan class klinikHewanContainer

--- app/src/main/java/com/example/a5_066/repository/klinikHewanContainer.kt
+++ app/src/main/java/com/example/a5_066/repository/klinikHewanContainer.kt
@@ -1,23 +1,30 @@
 package com.example.a5_066.repository
 
+import NetworkPasienRepository
+import PasienRepository
 import com.example.a5_066.service.DokterService
+import com.example.a5_066.service.JenisHewanService
 import com.example.a5_066.service.PasienService
+import com.example.a5_066.service.PerawatanService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 
-interface AppContainer{
+interface AppContainer {
     val pasienRepository: PasienRepository
     val dokterRepository: DokterRepository
+    val perawatanRepository: PerawatanRepository
+    val jenisHewanRepository: JenisHewanRepository
 }
 
-class klinikHewanContainer : AppContainer{
-    private val baseUrl ="http://10.0.2.2:8080/hewan/"//
+class klinikHewanContainer : AppContainer {
+    private val baseUrl = "http://10.0.2.2/hewan/" // Pastikan server dapat dijangkau
     private val json = Json { ignoreUnknownKeys = true }
     private val retrofit: Retrofit = Retrofit.Builder()
         .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
-        .baseUrl(baseUrl).build()
+        .baseUrl(baseUrl)
+        .build()
 
     private val pasienService: PasienService by lazy {
         retrofit.create(PasienService::class.java)
@@ -25,10 +32,23 @@ class klinikHewanContainer : AppContainer{
     private val dokterService: DokterService by lazy {
         retrofit.create(DokterService::class.java)
     }
+    private val perawatanService: PerawatanService by lazy {
+        retrofit.create(PerawatanService::class.java)
+    }
+    private val jenisHewanService:JenisHewanService by lazy {
+        retrofit.create(JenisHewanService::class.java)
+    }
+
     override val pasienRepository: PasienRepository by lazy {
         NetworkPasienRepository(pasienService)
     }
     override val dokterRepository: DokterRepository by lazy {
         NetworkDokterRepository(dokterService)
+    }
+    override val perawatanRepository: PerawatanRepository by lazy {
+        NetworkPerawatanRepository(perawatanService)
+    }
+    override val jenisHewanRepository: JenisHewanRepository by lazy {
+        NetworkJenisHewanRepository(jenisHewanService)
     }
 }
