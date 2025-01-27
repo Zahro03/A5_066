bdbb9a440d2f7211f7868161c6f619e17d998eb1
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-27 21:34:57 +0700

    Menambahkan interface DestinasiNavigasi

--- app/src/main/java/com/example/a5_066/navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/a5_066/navigation/DestinasiNavigasi.kt
@@ -1,57 +1,86 @@
-interface HalamanController {
+interface DestinasiNavigasi {
     val route: String
     val titleRes: String
 }
 
-object DestinasiHome : HalamanController {
+object DestinasiHomePage : DestinasiNavigasi{
+    override val route = "Home Page"
+    override val titleRes = "Home Page"
+}
+
+object DestinasiHomePasien : DestinasiNavigasi {
     override val route = "home"
-    override val titleRes = "Home"
+    override val titleRes = "Home Mhs"
 }
 
-object DestinasiHomePasien : HalamanController {
-    override val route = "pasien"
-    override val titleRes = "Pasien Home"
+object DestinasiHomejh : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Jenis Hewan"
 }
 
-object DestinasiInsertPasien : HalamanController {
-    override val route = "pasien/add"
+object DestinasiHomeDokter : DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = "Home Dokter"
+}
+
+object DestinasiHomePerawatan : DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = "Home Perawatan"
+}
+
+object DestinasiUpdatePasien : DestinasiNavigasi {
+    override val route = "updatePasien"
+    override val titleRes = "Edit Pasien"
+
+    const val id_hewan = "id_hewan"  // Ubah nama parameter untuk konsistensi
+    val routesWithArgs = "$route/{$id_hewan}"  // Mendefinisikan rute dengan argumen
+}
+
+object DestinasiUpdateDokter : DestinasiNavigasi {
+    override val route = "Update_dokter"
+    override val titleRes= "Edit Dokter"
+    const val id_Dokter = "id_Dokter"
+    val routeWithArgs = "$route/{$id_Dokter}"
+}
+
+object DestinasiUpdateJh : DestinasiNavigasi {
+    override val route = "Update_jenisHewan"
+    override val titleRes= "Edit Pasien"
+    const val id_jenis_hewan = "id_jenis_hewan"
+    val routeWithArgs = "$route/{$id_jenis_hewan}"
+}
+
+object DestinasiUpdatePerawatan : DestinasiNavigasi {
+    override val route = "Update_pasien"
+    override val titleRes= "Edit Pasien"
+    const val id_perawatan = "id_perawatan"
+    val routeWithArgs = "$route/{$id_perawatan}"
+}
+
+object DestinasiInsertJh : DestinasiNavigasi{
+    override val route = "Tambah_JenisHewan"
+    override val titleRes = "Tambah Jenis Hewan"
+}
+
+object DestinasiInsertPasien : DestinasiNavigasi{
+    override val route = "Tambah_Pasien"
     override val titleRes = "Tambah Pasien"
 }
 
-object DestinasiDetailPasien : HalamanController {
-    override val route = "pasien"
-    val id_hewan = "id_hewan"  // Ubah nama parameter untuk konsistensi
-    override val titleRes = "Detail Pasien"
-    val routesWithArg = "$route/{$id_hewan}"  // Mendefinisikan rute dengan argumen
-}
-
-object DestinasiUpdatePasien : HalamanController {
-    override val route = "updatePasien"
-    val id_hewan = "id_hewan"  // Ubah nama parameter untuk konsistensi
-    override val titleRes = "Ubah Pasien"
-    val routesWithArg = "$route/{$id_hewan}"  // Mendefinisikan rute dengan argumen
-}
-
-object DestinasiHomeDokter : HalamanController {
-    override val route = "dokter"
-    override val titleRes = "Dokter Home"
-}
-
-object DestinasiInsertDokter : HalamanController {
-    override val route = "dokter/add"
+object DestinasiInsertDokter : DestinasiNavigasi{
+    override val route = "Tambah_Dokter"
     override val titleRes = "Tambah Dokter"
 }
 
-object DestinasiDetailDokter : HalamanController {
-    override val route = "dokter"
-    val id_Dokter = "id_Dokter"
-    override val titleRes = "Detail Dokter"
-    val routesWithArg = "$route/{$id_Dokter}"  // Mendefinisikan rute dengan argumen
+object DestinasiInsertPerawatan : DestinasiNavigasi{
+    override val route = "Tambah_Perawatan"
+    override val titleRes = "Tambah Perawatan"
 }
 
-object DestinasiUpdateDokter : HalamanController {
-    override val route = "updateDokter"
-    val id_Dokter = "id_Dokter"
-    override val titleRes = "Ubah Dokter"
-    val routesWithArg = "$route/{$id_Dokter}"  // Mendefinisikan rute dengan argumen
+object DestinasiDetailPasien : DestinasiNavigasi {
+    override val route = "Detail_pasien"
+    override val titleRes = "Detail Pasien"
+    const val id_hewan = "id_hewan"
+    val routeWithArgs = "$route/{$id_hewan}"
 }
+
