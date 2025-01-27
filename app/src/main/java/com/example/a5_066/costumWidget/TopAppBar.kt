71a50891a8276a049cc6f2bd6eb4cd3f2f8abe8d
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-27 21:46:14 +0700

    Menambahkan fungsi TopAppBar

--- app/src/main/java/com/example/a5_066/costumWidget/TopAppBar.kt
+++ app/src/main/java/com/example/a5_066/costumWidget/TopAppBar.kt
@@ -1,6 +1,8 @@
 package com.example.a5_066.costumWidget
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Refresh
@@ -9,9 +11,16 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -22,18 +31,17 @@ fun CostumeTopAppBar(
     scrollBehavior: TopAppBarScrollBehavior? = null,
     navigateUp: () -> Unit = {},
     onRefresh: () -> Unit = {},
-    onBackPressed: () -> Unit,
-){
+) {
     CenterAlignedTopAppBar(
-
-        title = { Text(title) },
+        title = { Text(title)},
         actions = {
-            Icon(imageVector = Icons.Default.Refresh, contentDescription = "",modifier= Modifier.clickable {
+            Icon(imageVector = Icons.Default.Refresh, contentDescription = "", modifier = Modifier.clickable {
                 onRefresh()
             })
         },
+
         modifier = modifier,
-        scrollBehavior = scrollBehavior,navigationIcon = {
+        scrollBehavior = scrollBehavior, navigationIcon = {
             if(canNavigateBack){
                 IconButton(onClick = navigateUp) {
                     Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
