205b8f3454fe5855a8207c16875898ea5a2c57a4
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-27 21:26:39 +0700

    Menambahkan fungsi Repository Perawatan

--- app/src/main/java/com/example/a5_066/repository/PerawatanRepository.kt
+++ app/src/main/java/com/example/a5_066/repository/PerawatanRepository.kt
@@ -1,14 +1,17 @@
 package com.example.a5_066.repository
 
 import com.example.a5_066.model.Perawatan
+import com.example.a5_066.model.PerawatanResponse
+import com.example.a5_066.model.PerawatanResponseDetail
 import com.example.a5_066.service.PerawatanService
 import java.io.IOException
 
 interface PerawatanRepository {
-    suspend fun getRiwayatPerawatan(idHewan: String): List<Perawatan>
+    suspend fun getPerawatan(): PerawatanResponse
+    suspend fun getPerawatanById(idPerawatan: String): PerawatanResponseDetail
     suspend fun insertPerawatan(perawatan: Perawatan)
-    suspend fun updatePerawatan(idPerawatan: String, perawatan: Perawatan)
-    suspend fun deletePerawatan(idPerawatan: String)
+    suspend fun updatePerawatan(id_perawatan: String, perawatan: Perawatan)
+    suspend fun deletePerawatan(id_perawatan: String)
 }
 
 class NetworkPerawatanRepository(
@@ -16,37 +19,27 @@ class NetworkPerawatanRepository(
 ) : PerawatanRepository {
 
     override suspend fun insertPerawatan(perawatan: Perawatan) {
-        try {
-            perawatanApiService.insertPerawatan(perawatan)
-        } catch (e: Exception) {
-            throw IOException("Gagal mencatat perawatan: ${e.message}")
-        }
+        return perawatanApiService.insertPerawatan(perawatan)
     }
 
-    override suspend fun updatePerawatan(idPerawatan: String, perawatan: Perawatan) {
-        try {
-            perawatanApiService.updatePerawatan(idPerawatan, perawatan)
-        } catch (e: Exception) {
-            throw IOException("Gagal mengedit perawatan: ${e.message}")
-        }
+    override suspend fun updatePerawatan(id_perawatan: String, perawatan: Perawatan) {
+        return perawatanApiService.updatePerawatan(id_perawatan, perawatan)
     }
 
-    override suspend fun deletePerawatan(idPerawatan: String) {
-        try {
-            val response = perawatanApiService.deletePerawatan(idPerawatan)
+    override suspend fun deletePerawatan(id_perawatan: String) {
+            val response = perawatanApiService.deletePerawatan(id_perawatan)
             if (!response.isSuccessful) {
                 throw IOException("Gagal menghapus perawatan. HTTP Status Code: ${response.code()}")
-            }
-        } catch (e: Exception) {
-            throw IOException("Gagal menghapus perawatan: ${e.message}")
+            } else {
+                println(response.message())
         }
     }
 
-    override suspend fun getRiwayatPerawatan(idHewan: String): List<Perawatan> {
-        return try {
-            perawatanApiService.getRiwayatPerawatan(idHewan)
-        } catch (e: Exception) {
-            throw IOException("Gagal mendapatkan riwayat perawatan: ${e.message}")
-        }
+    override suspend fun getPerawatan(): PerawatanResponse {
+        return perawatanApiService.getPerawatan()
+    }
+
+    override suspend fun getPerawatanById(id_perawatan: String): PerawatanResponseDetail {
+        return perawatanApiService.getPerawatanById(id_perawatan)
     }
 }
