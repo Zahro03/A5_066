2e0d957c762b4e9cacc6b66971a3d8b6331beac6
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-27 21:25:47 +0700

    Membuat fungsi Repository Pasien

--- app/src/main/java/com/example/a5_066/repository/PasienRepository.kt
+++ app/src/main/java/com/example/a5_066/repository/PasienRepository.kt
@@ -1,15 +1,12 @@
-package com.example.a5_066.repository
-
-import Pasien
 import com.example.a5_066.service.PasienService
 import java.io.IOException
 
 interface PasienRepository {
-    suspend fun getPasien(): List<Pasien>
-    suspend fun getPasienById(id: String): Pasien
+    suspend fun getPasien(): PasienResponse
+    suspend fun getPasienById(id_hewan: String): PasienResponseDetail
     suspend fun insertPasien(pasien: Pasien)
-    suspend fun updatePasien(id: String, pasien: Pasien)
-    suspend fun deletePasien(id: String)
+    suspend fun updatePasien(id_hewan: String, pasien: Pasien)
+    suspend fun deletePasien(id_hewan: String)
 }
 
 class NetworkPasienRepository(
@@ -17,31 +14,27 @@ class NetworkPasienRepository(
 ) : PasienRepository {
 
     override suspend fun insertPasien(pasien: Pasien) {
-        pasienApiService.insertPasien(pasien)
+        pasienApiService.insertPasien(pasien) // Pastikan API call ini benar
     }
 
-    override suspend fun updatePasien(id: String, pasien: Pasien) {
-        pasienApiService.updatePasien(id,pasien)
+    override suspend fun updatePasien(id_hewan: String, pasien: Pasien) {
+        pasienApiService.updatePasien(id_hewan, pasien)
     }
 
-    override suspend fun deletePasien(id: String) {
-        try {
-            val response = pasienApiService.deletePasien(id)
+    override suspend fun deletePasien(id_hewan: String) {
+            val response = pasienApiService.deletePasien(id_hewan)
             if (!response.isSuccessful) {
-                throw IOException(
-                    "Failed to delete pasien. HTTP Status Code: " +
-                            "${response.code()}"
-                )
+                throw IOException("Gagal menghapus pasien. Kode Status HTTP: ${response.code()}")
             } else {
-                response.message()
                 println(response.message())
             }
-        } catch (e: Exception) {
-            throw e
         }
+
+    override suspend fun getPasien(): PasienResponse {
+        return pasienApiService.getPasien()
     }
-    override suspend fun getPasien(): List<Pasien> = pasienApiService.getPasien()
-    override suspend fun getPasienById(id: String): Pasien {
-        return pasienApiService.getPasienById(id)
+
+    override suspend fun getPasienById(id_hewan: String): PasienResponseDetail {
+        return pasienApiService.getPasienById(id_hewan)
     }
 }
