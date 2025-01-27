7c9a76aa30390b471f3f6d541dfe0184ea381cb1
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-27 21:24:27 +0700

    Membuat fungsi Repositori Jenis Hewan

--- app/src/main/java/com/example/a5_066/repository/JenisHewanRepository.kt
+++ app/src/main/java/com/example/a5_066/repository/JenisHewanRepository.kt
@@ -1,48 +1,48 @@
 package com.example.a5_066.repository
 
 import com.example.a5_066.model.JenisHewan
+import com.example.a5_066.model.JenisHewanResponse
+import com.example.a5_066.model.JenisHewanResponseDetail
+import com.example.a5_066.service.JenisHewanService
 import kotlinx.coroutines.flow.Flow
 import okio.IOException
 
 interface JenisHewanRepository{
-    suspend fun getJenisHewan(): List<JenisHewan>
+    suspend fun getJenisHewan(): JenisHewanResponse
     suspend fun insertJenisHewan(jenisHewan: JenisHewan)
-    suspend fun updateJenisHewan(id: String,jenisHewan: JenisHewan)
-    suspend fun deleteJenisHewan(id: String)
-    suspend fun getJenisHewanById(id: String):JenisHewan
+    suspend fun updateJenisHewan(id_jenis_hewan: String,jenisHewan: JenisHewan)
+    suspend fun deleteJenisHewan(id_jenis_hewan: String)
+    suspend fun getJenisHewanById(id_jenis_hewan: String): JenisHewanResponseDetail
 }
 
 class NetworkJenisHewanRepository(
-    private val JenisHewanApiService: JenisHewanService
+    private val jenisHewanApiService: JenisHewanService
 ): JenisHewanRepository{
     override suspend fun insertJenisHewan(jenisHewan: JenisHewan){
-        JenisHewanApiService.insertJenisHewan(jenisHewan)
+        jenisHewanApiService.insertJenisHewan(jenisHewan)
     }
 
-    override suspend fun updateJenisHewan(id: String,jenisHewan: JenisHewan) {
-        JenisHewanApiService.updateJenisHewan(id, jenisHewan)
+    override suspend fun updateJenisHewan(id_jenis_hewan: String,jenisHewan: JenisHewan) {
+        jenisHewanApiService.updateJenisHewan(id_jenis_hewan, jenisHewan)
     }
 
-    override suspend fun deleteJenisHewan(id: String) {
-        try {
-            val response = JenisHewanApiService.deleteJenisHewan(id)
-            if (!response.isSuccessful){
-                throw java.io.IOException(
-                    "Failed to delete jenisHewan.HTTP Status code:" +
-                            "${response.code()}"
-                )
-            } else {
-                response.message()
-                println(response.message())
-            }
-        } catch (e:Exception){
-            throw e
+    override suspend fun deleteJenisHewan(id_jenis_hewan: String) {
+        val response = jenisHewanApiService.deleteJenisHewan(id_jenis_hewan)
+        if (!response.isSuccessful){
+            throw java.io.IOException(
+                "Failed to delete jenisHewan.HTTP Status code:" +
+                        "${response.code()}"
+            )
+        } else {
+            println(response.message())
         }
     }
 
-    override suspend fun getJenisHewan(): List<JenisHewan> = JenisHewanApiService.getJenisHewan()
-    override suspend fun getJenisHewanById(id: String): JenisHewan {
-        return JenisHewanApiService.getMahasiwaById(id)
+    override suspend fun getJenisHewan(): JenisHewanResponse {
+        return jenisHewanApiService.getJenisHewan()
+    }
+    override suspend fun getJenisHewanById(id_jenis_hewan: String): JenisHewanResponseDetail {
+        return jenisHewanApiService.getJenisHewanById(id_jenis_hewan)
     }
 }
 
