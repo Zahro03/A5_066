023b22496d78e8ef6240b6ede6e8c19e74f29bd6
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-27 21:32:58 +0700

    Menambahkan fungsi pada UpdateView Pasien

--- app/src/main/java/com/example/a5_066/ui/view/pasien/UpdateView.kt
+++ app/src/main/java/com/example/a5_066/ui/view/pasien/UpdateView.kt
@@ -1,56 +1,59 @@
 package com.example.a5_066.ui.view.pasien
 
-import HalamanController
+import DestinasiUpdatePasien
+import UpdatePasienViewModel
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.a5_066.costumWidget.CostumeTopAppBar
-import com.example.a5_066.ui.viewModel.PenyediaViewModel
-import com.example.a5_066.ui.viewModel.pasien.UpdateViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-
-object DestinasiUpdate : HalamanController {
-    override val route = "Update_pasien"
-    override val titleRes= "Edit Pasien"
-    const val id_hewan = "id_hewan"
-    val routeWithArgs = "$route/{$id_hewan}"
-}
+import kotlinx.coroutines.withContext
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UpdatePasienScreen(
-    navigateBack: () -> Unit,
-    onNavigateUp: () -> Unit,
+    onBack: () -> Unit,
     modifier: Modifier = Modifier,
-    updateViewModel: UpdateViewModel = viewModel(factory = PenyediaViewModel.Factory)
-) {
+    onNavigate: () -> Unit,
+    viewModel: UpdatePasienViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    id_hewan: String
+){
     val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
-    Scaffold(
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
             CostumeTopAppBar(
-                title = "Edit Pasien (Hewan)",
+                title = DestinasiUpdatePasien.titleRes,
                 canNavigateBack = true,
-                navigateUp = navigateBack,
-                onBackPressed = navigateBack
+                scrollBehavior = scrollBehavior,
+                navigateUp = onBack,
             )
-        },
-        modifier = modifier
-    ) { innerPadding ->
+        }
+    ){padding ->
         EntryBody(
-            insertUiState = updateViewModel.updatePasienUiState,
-            onPasienValueChange = updateViewModel::updateInsertPasienState,
+            modifier = Modifier.padding(padding),
+            insertPasienUiState = viewModel.updateUiState,
+            onPasienValueChange = viewModel::updateState,
+            jenisList = viewModel.jenisList,
             onSaveClick = {
                 coroutineScope.launch {
-                    updateViewModel.updatePasien()
-                    onNavigateUp()
+                    viewModel.updatePasien()
+                    delay(600)
+                    withContext(Dispatchers.Main){
+                        onNavigate()
+                    }
                 }
-            },
-            modifier = Modifier.padding(innerPadding)
+            }
         )
     }
 }
