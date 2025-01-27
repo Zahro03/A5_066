648e72f7f765c8857d33fcc3b252f6bd80e72824
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-27 21:25:30 +0700

    Membuat class Pasien

--- app/src/main/java/com/example/a5_066/model/Pasien.kt
+++ app/src/main/java/com/example/a5_066/model/Pasien.kt
@@ -7,12 +7,36 @@ data class Pasien(
     val nama_hewan: String,
     val jenis_hewan_id: String,
     val pemilik: String,
-
-    @SerialName("kontak_pemilik")
     val kontak_pemilik: String,
-    @SerialName("tanggal_lahir")
     val tanggal_lahir: String,
-    @SerialName("catatan_kesehatan")
     val catatan_kesehatan: String
 )
 
+@Serializable
+data class PasienResponse(
+    val status : Boolean,
+    val message : String,
+    val data : List<Pasien>
+)
+
+@Serializable
+data class PasienResponseDetail(
+    val status: Boolean,
+    val message: String,
+    val data: Pasien
+) {
+    // Fungsi untuk mengonversi Pasien menjadi InsertPasienUiState
+    fun toUiStatePsn(): InsertPasienUiState {
+        return InsertPasienUiState(
+            insertPasienUiEvent = InsertPasienUiEvent(
+                id_hewan = data.id_hewan,
+                nama_hewan = data.nama_hewan,
+                jenis_hewan_id = data.jenis_hewan_id,
+                pemilik = data.pemilik,
+                kontak_pemilik = data.kontak_pemilik,
+                tanggal_lahir = data.tanggal_lahir,
+                catatan_kesehatan = data.catatan_kesehatan
+            )
+        )
+    }
+}
