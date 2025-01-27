c81bd8b1e73f99de70dcc5ef2c2d530881dc973e
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-27 21:20:02 +0700

    Membuat interface DokterService

--- app/src/main/java/com/example/a5_066/service/DokterService.kt
+++ app/src/main/java/com/example/a5_066/service/DokterService.kt
@@ -1,6 +1,8 @@
 package com.example.a5_066.service
 
 import com.example.a5_066.model.Dokter
+import com.example.a5_066.model.DokterResponse
+import com.example.a5_066.model.DokterResponseDetail
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
@@ -8,6 +10,7 @@ import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.PUT
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface DokterService {
@@ -17,19 +20,19 @@ interface DokterService {
     )
     // Menampilkan daftar pasien
     @GET("bacadokter.php")
-    suspend fun getDokter(): List<Dokter>
+    suspend fun getDokter(): DokterResponse
 
     // Menampilkan detail pasien berdasarkan ID
     @GET("baca1dokter.php")
-    suspend fun getDokterById(@Query("id_dokter") id: String): Dokter
+    suspend fun getDokterById(@Path("id_dokter") id: String): DokterResponseDetail
 
     // Menambahkan data pasien baru
     @POST("insertdokter.php")
-    suspend fun insertDokter(@Body dokter: Dokter): Dokter
+    suspend fun insertDokter(@Body dokter: Dokter)
 
     // Memperbarui data pasien
     @PUT("editdokter.php")
-    suspend fun updateDokter(@Query("id_dokter") id: String, @Body dokter: Dokter)
+    suspend fun updateDokter(@Path("id_dokter") id: String, @Body dokter: Dokter)
 
     // Menghapus data pasien berdasarkan ID
     @DELETE("deletedokter.php/{id_dokter}")
