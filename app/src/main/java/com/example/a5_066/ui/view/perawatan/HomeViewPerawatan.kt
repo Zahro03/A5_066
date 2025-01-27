1af18deef95bef1a4801c2edb630f2f650a98740
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-27 19:31:24 +0700

    Membuat fungsi untuk HomePerawatanScreen

--- app/src/main/java/com/example/a5_066/ui/view/perawatan/HomeViewPerawatan.kt
+++ app/src/main/java/com/example/a5_066/ui/view/perawatan/HomeViewPerawatan.kt
@@ -1,34 +1,16 @@
 package com.example.a5_066.ui.view.perawatan
 
-import HalamanController
+import DestinasiNavigasi
+import HomeUiState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -36,88 +18,86 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.a5_066.R
-import com.example.a5_066.costumWidget.TopAppBar
+import com.example.a5_066.costumWidget.CostumeTopAppBar
 import com.example.a5_066.model.Perawatan
-import com.example.a5_066.ui.viewModel.perawatan.PerawatanUiState
+import com.example.a5_066.ui.viewModel.perawatan.HomePrUiState
 import com.example.a5_066.ui.viewModel.perawatan.PerawatanViewModel
 
-object DestinasiHomePerawatan : HalamanController {
+object DestinasiHomePerawatan : DestinasiNavigasi {
     override val route = "perawatan"
-    override val titleRes = "Perawatan Hewan"
+    override val titleRes = "Home Perawatan"
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun PerawatanScreen(
-    navigateToAddPerawatan: () -> Unit,
+fun HomePerawatanScreen(
+    navigateToItemEntry: () -> Unit,
     modifier: Modifier = Modifier,
     onDetailClick: (String) -> Unit = {},
-    viewModel: PerawatanViewModel = androidx.lifecycle.viewmodel.compose.viewModel() // Menggunakan viewModel() untuk mendapatkan ViewModel
+    viewModel: PerawatanViewModel = viewModel()
 ) {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     Scaffold(
         modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-            TopAppBar(
+            CostumeTopAppBar(
                 title = DestinasiHomePerawatan.titleRes,
                 canNavigateBack = false,
                 scrollBehavior = scrollBehavior,
-                onRefresh = {
-                    viewModel.getRiwayatPerawatan("id_hewan")  // Ganti dengan ID Hewan yang sesuai
-                }
+                onRefresh = { viewModel.getPerawatan() }
             )
         },
         floatingActionButton = {
             FloatingActionButton(
-                onClick = navigateToAddPerawatan,
+                onClick = navigateToItemEntry,
                 shape = MaterialTheme.shapes.medium,
                 modifier = Modifier.padding(18.dp)
             ) {
-                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Perawatan")
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Tambah Perawatan")
             }
-        },
+        }
     ) { innerPadding ->
-        PerawatanStatus(
-            perawatanUiState = viewModel.perawatanUIState,
-            retryAction = { viewModel.getRiwayatPerawatan("id_hewan") },
+        HomeStatus(
+            homePrUiState = viewModel.perawatanUIState,
+            retryAction = { viewModel.getPerawatan() },
             modifier = Modifier.padding(innerPadding),
             onDetailClick = onDetailClick,
-            onDeleteClick = {
-                viewModel.deletePerawatan(it.id_perawatan) // Hapus perawatan berdasarkan ID
-                viewModel.getRiwayatPerawatan("id_hewan")  // Refresh data perawatan setelah dihapus
+            onDeleteClick = { perawatan ->
+                viewModel.deletePerawatan(perawatan.id_perawatan)
+                viewModel.getPerawatan()
             }
         )
     }
 }
 
 @Composable
-fun PerawatanStatus(
-    perawatanUiState: PerawatanUiState,
+fun HomeStatus(
+    homePrUiState: HomePrUiState,
     retryAction: () -> Unit,
     modifier: Modifier = Modifier,
     onDeleteClick: (Perawatan) -> Unit = {},
     onDetailClick: (String) -> Unit
 ) {
-    when (perawatanUiState) {
-        is PerawatanUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
-        is PerawatanUiState.Success ->
-            if (perawatanUiState.perawatan.isEmpty()) {
+    when (homePrUiState) {
+        is HomePrUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+        is HomePrUiState.Success -> {
+            if (homePrUiState.perawatan.isEmpty()) {
                 Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
                     Text(text = "Tidak ada data perawatan")
                 }
             } else {
                 PerawatanLayout(
-                    perawatan = perawatanUiState.perawatan, modifier = modifier.fillMaxWidth(),
-                    onDetailClick = {
-                        onDetailClick(it.id_perawatan)
-                    },
-                    onDeleteClick = {
-                        onDeleteClick(it)
-                    }
+                    perawatan = homePrUiState.perawatan,
+                    modifier = modifier.fillMaxWidth(),
+                    onDetailClick = { onDetailClick(it.id_perawatan) },
+                    onDeleteClick = { onDeleteClick(it) }
                 )
             }
-        is PerawatanUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+        }
+        is HomePrUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+        else -> {}
     }
 }
 
@@ -137,9 +117,7 @@ fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.loading_failed), contentDescription = ""
-        )
+        Image(painter = painterResource(id = R.drawable.loading_failed), contentDescription = "")
         Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
         Button(onClick = retryAction) {
             Text(stringResource(R.string.retry))
@@ -165,9 +143,7 @@ fun PerawatanLayout(
                 modifier = Modifier
                     .fillMaxWidth()
                     .clickable { onDetailClick(perawatan) },
-                onDeleteClick = {
-                    onDeleteClick(perawatan)
-                }
+                onDeleteClick = { onDeleteClick(perawatan) }
             )
         }
     }
@@ -201,16 +177,13 @@ fun PerawatanCard(
                 }
             }
             Text(
-                text = "Tanggal: ${perawatan.tanggal_perawatan}",
+                text = "Tanggal Perawatan: ${perawatan.tanggal_perawatan}",
                 style = MaterialTheme.typography.titleMedium
             )
             Text(
-                text = "Deskripsi: ${perawatan.detail_perawatan}",
+                text = "Detail Perawatan: ${perawatan.detail_perawatan}",
                 style = MaterialTheme.typography.bodyMedium
             )
         }
     }
 }
-
-
-
