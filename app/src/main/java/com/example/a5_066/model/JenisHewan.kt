31e5aa5bec36884986665c811a53ea18426c72a5
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2025-01-27 21:24:04 +0700

    Membuat class JenisHewan

--- app/src/main/java/com/example/a5_066/model/JenisHewan.kt
+++ app/src/main/java/com/example/a5_066/model/JenisHewan.kt
@@ -8,3 +8,17 @@ data class JenisHewan(
     val nama_jenis_hewan: String,
     val deskripsi: String
 )
+
+@Serializable
+data class JenisHewanResponse(
+    val status : Boolean,
+    val message : String,
+    val data : List<JenisHewan>
+)
+
+@Serializable
+data class JenisHewanResponseDetail(
+    val status : Boolean,
+    val message : String,
+    val data : JenisHewan
+)
